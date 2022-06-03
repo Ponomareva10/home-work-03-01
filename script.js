3cc407d30cb597a9fd482d8debbb16714c3c48c7
Ponomareva10 <105550506+Ponomareva10@users.noreply.github.com>
2022-06-03 10:19:36 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let a = 10;
+let b = 3;
+const rem = a % b;
+console.log(rem);
+
+let i = 2;
+let sa = 10;
+const st = i ** sa;
+console.log(st);
+
+const ai = '1';
+const bi = '2';
+const ci = ' белых медведей';
+const sum = ai + bi + ci;
+console.log(sum);
+
+let aa = 5;
+let bb = 10;
+bb = --bb
+let cc = ++aa + bb--;
+console.log(cc);
+
+let x = 15 * 4 + 25 - 55;
+x = 15 * ( 4 + 25 - 55 );
+console.log(x);
+
+const height = 23;
+const width = 10;
+const s = height * width;
+console.log(s);
+
+const heightC = 10;
+const dC = 4;
+const pC = 3.1415;
+const v  = pC * heightC * ( dC ** 2);
+console.log(v);
+
+const S = 2000000;
+const p = 10;
+const years = 5;
+const perepl = S / p * years
+console.log(perepl);
